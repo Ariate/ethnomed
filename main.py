eb0403655f37ecb30136973e003735d695fff23b
Reg <reg.dipasupil344@gmail.com>
2022-04-20 02:48:42 +0800

    Renamed file 2 electric boogaloo

---
+++ main.py
@@ -0,0 +1,25 @@
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
+import datetime
+from localpackage.PlantProgramRes import Ethnomed
+
+
+def main():  
+
+    cred=credentials.Certificate(r'C:\Users\regdi\Documents\reg documents\Miscellaneous\Mark Desperation\ethnomed\firebasekey.json')
+    firebase_admin.initialize_app(cred, {
+        'storageBucket': 'ethnomed-de562.appspot.com'
+    })
+    db = firestore.client()
+
+    bucket = storage.bucket()
+    blob = bucket.blob("Guava.jpg")
+    image_url = blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET')
+    
+    Ethnomed(str(image_url))
+    
+    blob = bucket.blob("psd_img.png")
+    blob.upload_from_filename("psd_img.png")
+
+if __name__ == "__main__":
+    main();
