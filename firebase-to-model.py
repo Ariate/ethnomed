c739e0510b1e7ec3f0341e552628829dba7a9395
Reg <reg.dipasupil344@gmail.com>
2022-04-20 01:31:16 +0800

    Updated the FUCKING CODE 30 MINUTES FOR NOTHING

--- firebase-to-model.py
+++ firebase-to-model.py
@@ -1,14 +1,24 @@
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, firestore, storage
+import datetime
+import requests
 
-cred = credentials.Certificate("firebasekey.json")
-firebase_admin.initialize_app(cred)
 
-app = firebase_admin.initialize_app(cred, {
-    'storageBucket': 'gs://ethnomed-de562.appspot.com/',
-}, name='storage')
+def main():  
 
-bucket = storage.bucket(app=app)
-blob = bucket.blob("puzzle-piece-template-13.png")
+    cred=credentials.Certificate(r'C:\Users\regdi\Documents\reg documents\Miscellaneous\Mark Desperation\ethnomed\firebasekey.json')
+    firebase_admin.initialize_app(cred, {
+        'storageBucket': 'ethnomed-de562.appspot.com'
+    })
+    db = firestore.client()
 
-print(blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET'))
+    bucket = storage.bucket()
+    blob = bucket.blob("puzzle-piece-template-13.png")
+    image_url = blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET')
+    
+    img_data = requests.get(image_url).content
+    with open('puzzle-piece-template-13.png', 'wb') as handler:
+        handler.write(img_data)
+
+if __name__ == "__main__":
+    main();
