45796fcdc95e00f17798e53fde0468704473b164
Reg <reg.dipasupil344@gmail.com>
2022-04-20 13:30:34 +0800

    Worked in local

--- localpackage/PlantProgramRes.py
+++ localpackage/PlantProgramRes.py
@@ -1,11 +1,13 @@
+# Put back imports to function when going to github
 def Ethnomed(img_str):
+    
     import numpy as np
     from skimage import io
     from cv2 import cv2
     import io as io2
     import base64
-
     plant_cascade=cv2.CascadeClassifier(r'./localpackage/cascade.xml')
+    # plant_cascade=cv2.CascadeClassifier(r'C:\Users\regdi\Documents\reg documents\Miscellaneous\Mark Desperation\ethnomed\localpackage\cascade.xml')
 
     im_bytes = base64.b64decode(img_str)
     im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
@@ -21,9 +23,19 @@ def Ethnomed(img_str):
         roi_color = img[y:y+h, x:x+w]
     
     is_success, buffer = cv2.imencode(".jpg", img)
-    byte_img = buffer.tobytes()
+    psd_img = base64.b64encode(buffer)
 
     
-    return byte_img
+    return psd_img
+
+# Del
+#with open(r"C:\Users\regdi\Documents\reg documents\Miscellaneous\Mark Desperation\raw_img.jpg", "rb") as f:
+#    im_bytes = f.read()
+
+#im_b64 = base64.b64encode(im_bytes).decode("utf8")
+# Del
+
+#with open("imageToSave.png", "wb") as fh:
+#    fh.write(base64.decodebytes(Ethnomed(im_b64)))
 
     
